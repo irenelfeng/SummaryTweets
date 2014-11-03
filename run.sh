9a3f64354d4b1664ede14e37978123c224631748
Patrick Xu <xu.17@dartmouth.edu>
2014-11-03 11:25:44 -0500

    adjusted arguments to match corpus folder structure

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ filename=tf_idf.py
 flag1=-text
 arg1="Hello Irene, Orestis, and Pat"
 flag2=-c
-arg2=./CorpusFolder/brown/ca/
+arg2=./CorpusFolder/
+flag3=-tagged
+arg3=True
 
-python $filename $flag1 "$arg1" $flag2 $arg2
+python $filename $flag1 "$arg1" $flag2 $arg2 $flag3 $arg3
