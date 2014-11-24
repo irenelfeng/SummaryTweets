bc80bba3192fb90e6e8da6335149bdc8b6390c88
irenelfeng <irene.l.feng.17@dartmouth.edu>
2014-11-23 21:05:15 -0500

    added phrases with probabilities: dictionary of dictionaries

--- picklephrase.py
+++ picklephrase.py
@@ -17,23 +17,31 @@ if __name__=='__main__':
 		for line in paraphrase:
 			line = line.split('|||')
 			word = line[2].strip() #target e
-			otherWord = line[1].strip() #source f
-			if len(word) > len(otherWord): #if e > f
+			other_word = line[1].strip() #source f
+			if len(word) > len(other_word): #if e > f
 				#print word 
 				#print otherWord
-				#m = re.search('\sp\(e\|f\)=([0-9.]+)', line[3]) #get prob(f|e), probability of shorter given longer.
+				m = re.search('\sp\(e\|f\)=([0-9.]+)', line[3]) #get prob(f|e), probability of shorter given longer.
 				#print m.group(1)
 				#print line
-				#phrase_dict[(word, otherword)] = ###WILL ADD 
-				phrase_dict[word] = otherWord
-			elif len(otherWord) > len(word): #if f > e
-				#m = re.search('\sp\(e\|f\)=([0-9.]+)', line[3]) #get prob(f|e), probability of shorter given longer.
-				phrase_dict[otherWord] = word
+				if (word not in phrase_dict): 
+					phrase_dict[word] = {}
+				phrase_dict[word][other_word] = m.group(1)
+				#phrase_dict[(word, other_word)] = m.group(1) #add prob(e|f) to the phrase dict
+				#phrase_dict[word] = otherWord
+			elif len(other_word) > len(word): #if f > e
+				m = re.search('\sp\(f\|e\)=([0-9.]+)', line[3]) #get prob(f|e), probability of shorter given longer.
+				#phrase_dict[otherWord] = word
+				if (other_word not in phrase_dict): 
+					phrase_dict[other_word] = {}
+				phrase_dict[other_word][word] = m.group(1)
+				#phrase_dict[(other_word, word)] = m.group(1)  #get prob(f|e)
 			testPos.add(line[0])
 		paraphrase.close()
-	for pos in testPos:
-		print pos
-	pickl('pickl/allPhrases')
-	pickled = open('pickl/allPhrases', 'r')
+	#for pos in testPos:
+		#print pos
+	pickl('pickl/allPhrasesProb')
+	pickled = open('pickl/allPhrasesProb', 'r')
 	phrases = pickle.load(pickled)
-	#print phrases
+	for key, value in phrases.iteritems() :
+		print key, value
